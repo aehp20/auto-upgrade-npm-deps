7c27a8ff0f534c9e58369d302bc75eb5f1ace923
Aron HERRERA PONTE <aronherrera2009@gmail.com>
2024-08-31 18:34:52 +0200

    use eslint-config-prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,9 @@
 import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
 import globals from "globals";
 
 export default [
+  eslintConfigPrettier,
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   {
