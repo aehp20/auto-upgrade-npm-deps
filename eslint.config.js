80153370646204bfc0e30c4cfa6600443fbc11ff
Aron HERRERA PONTE <aronherrera2009@gmail.com>
2024-08-31 19:27:32 +0200

    update prettier command

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,17 @@
-import pluginJs from "@eslint/js";
-import eslintConfigPrettier from "eslint-config-prettier";
-import globals from "globals";
+import pluginJs from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import globals from 'globals';
 
 export default [
   eslintConfigPrettier,
-  {languageOptions: { globals: globals.browser }},
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   {
     files: ['**/*.{js,jsx,ts,tsx}'],
     rules: {
-        "no-console": "warn",
-        "eqeqeq": "error",
-        "capitalized-comments": "error"
-    }
-  }
+      'no-console': 'warn',
+      eqeqeq: 'error',
+      'capitalized-comments': 'error',
+    },
+  },
 ];
